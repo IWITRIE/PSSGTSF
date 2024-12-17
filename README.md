5e5243b9dd4e7b7b1ef7d09fafd573d86283ba5c
IWITRIE <2310428@mail.nankai.edu.cn>
2024-12-18 00:10:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PSSGTSF
+
+此模型是我们基于SparseTSF模型的改进模型
+
+## 运行
+```
+conda create -n PSSGTSF python=3.8
+conda activate PSSGTSF
+pip install -r requirements.txt
+sh run_all.sh
+```
+## 目录结构
+
---
+++ run_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# linear
+# sh scripts/SparseTSF/linear/etth1.sh;
+# sh scripts/SparseTSF/linear/etth2.sh;
+# sh scripts/SparseTSF/linear/electricity.sh;
+# sh scripts/SparseTSF/linear/traffic.sh;
+# sh scripts/SparseTSF/linear/ettm1.sh;
+# sh scripts/SparseTSF/linear/ettm2.sh;
+# sh scripts/SparseTSF/linear/weather.sh;
+# sh scripts/SparseTSF/linear/solar.sh;
+
+
+# mlp
+# sh scripts/SparseTSF/mlp/etth1.sh;
+# sh scripts/SparseTSF/mlp/etth2.sh;
+# sh scripts/SparseTSF/mlp/electricity.sh;
+# sh scripts/SparseTSF/mlp/traffic.sh;
+# sh scripts/SparseTSF/mlp/ettm1.sh;
+# sh scripts/SparseTSF/mlp/ettm2.sh;
+# sh scripts/SparseTSF/mlp/weather.sh;
+# sh scripts/SparseTSF/mlp/solar.sh;
+
+
+
+sh scripts/PSSGTSF/graph/etth1.sh;
+sh scripts/PSSGTSF/graph/etth2.sh;
+sh scripts/PSSGTSF/graph/electricity.sh;
+sh scripts/PSSGTSF/graph/traffic.sh;
+sh scripts/PSSGTSF/graph/ettm1.sh;
+sh scripts/PSSGTSF/graph/ettm2.sh;
+sh scripts/PSSGTSF/graph/weather.sh;
